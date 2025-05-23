f98275fde97a33736d646b423b3c250e58bde02e
Palta06 <bastian.guerra.v@mail.pucv.cl>
2025-05-23 19:52:20 -0400

    game.h

--- game.h
+++ game.h
@@ -1,1 +1,25 @@
+// game.h
+#ifndef GAME_H
+#define GAME_H
 
+#include "list.h"
+#include "grafo.h"
+
+typedef struct {
+    List* inventory;    // inventario único del jugador
+    int tiempoRestante; // tiempo que le queda al jugador
+    Node* currentNode;  // escenario actual
+} GameState;
+
+void iniciar_partida(void);
+
+void mostrar_puntaje_final(GameState* gs);
+void mostrar_estado_actual(GameState* gs);
+void showGameOptions(void);
+char readOption(char* buf, int len);
+void recoger_items(GameState* gs);
+void descartar_items(GameState* gs);
+void moverse(GameState* gs);
+
+
+#endif // GAME_H
