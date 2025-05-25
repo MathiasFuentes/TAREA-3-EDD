d2ffdebd3e2fc283f2a90f38559f2c897f8f69c2
Palta06 <bastian.guerra.v@mail.pucv.cl>
2025-05-25 02:00:17 -0400

    grafo.c

--- grafo.c
+++ grafo.c
@@ -1,11 +1,12 @@
 #include "grafo.h"
 #include "extra.h"
 #include "list.h"
+#include "game.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include "game.h"
 
 Graph graph;
 
