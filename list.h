eeee2de264b5a60278ff3808ec2d0a5da4b13698
Palta06 <bastian.guerra.v@mail.pucv.cl>
2025-05-25 02:02:16 -0400

    list.h

--- list.h
+++ list.h
@@ -3,14 +3,14 @@
 
 typedef struct List List;
 
-List* list_create();
-void list_pushBack(List* list, void* data);
 void* list_first(List* list);
 void* list_next(List* list);
+void* list_current(List* list);
+void list_pushBack(List* list, void* data);
+void list_popCurrent(List* list);
+int list_size(List* list);
+List* list_create();
 void list_clean(List* list);
 void list_destroy(List* list);
-int list_size(List* list);
-void* list_current(List* list);
-void list_popCurrent(List* list);
 
 #endif
